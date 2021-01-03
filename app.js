23fae86a197bce1fa4c6286141a27349f718496b
rluciver <rluciver@aol.com>
2021-01-03 15:49:07 +0700

    pebaikan connection ke database mongo db

--- app.js
+++ app.js
@@ -3,17 +3,23 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-var app = express();
 
+const cors = require("cors");
+const productRouter = require("./app/products/router");
+
+var app = express();
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api", productRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- database/index.js
+++ database/index.js
@@ -2,8 +2,13 @@ const mongoose = require("mongoose");
 const { dbHost, dbName, dbPort, dbUser, dbPass } = require("../app/config");
 
 mongoose.connect(
-  `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`,
-  { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }
+  `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`,
+  // "mongodb://ExtraSuperUser:Dausganteng12@localhost:27017/latihan?authSource=admin",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  }
 );
 
 const db = mongoose.connection;
