d6833615671b2cb7b1c38d106bdb22c006f386f7
rluciver <rluciver@aol.com>
2021-01-06 22:10:06 +0700

    Pembuatan Model & Schema CartItem

---
+++ app/cart/model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const { model, Schema } = mongoose.set("useCreateIndex", true);
+
+const cartItemSchema = Schema({
+  name: {
+    type: String,
+    minlength: [5, "panjang nama makanan minimal 5 karakter"],
+    require: [true, "name harus di isi"],
+  },
+  qty: {
+    type: Number,
+    require: [true, "qty harus di isi"],
+    min: [1, "minimal qty adalah 1"],
+  },
+  price: {
+    type: Number,
+    default: 0,
+  },
+
+  image_url: String,
+
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  product: {
+    type: Schema.Types.ObjectId,
+    ref: "Product",
+  },
+});
+
+module.exports = model("CartItem", cartItemSchema);
