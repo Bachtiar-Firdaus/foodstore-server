baf0bce3e8a05fcdd109a9190963fbcd6459e80b
rluciver <rluciver@aol.com>
2021-01-07 13:38:08 +0700

    pembuatan virtual field items count pada schema order

--- app/order/model.js
+++ app/order/model.js
@@ -31,4 +31,10 @@ const orderSchema = Schema(
 
 orderSchema.plugin(AutoIncrement, { inc_field: "order_number" });
 
+orderSchema.virtual("items_count").get(function () {
+  return this.order_items.reduce((total, item) => {
+    return total + parseInt(item.qty);
+  }, 0);
+});
+
 module.exports = model("Order", orderSchema);
